7ed21253c28316aeb351039929ac8fd0fa5a3969
jmt <jmt>
2015-03-15 17:45:22 -0500

    quick

--- src/blink.c
+++ src/blink.c
@@ -246,9 +246,8 @@ void setPixel(int x, int y)
 	int width = 127;
 	int height = 63;
 
-	if (x < 0 || y < 0 || x > width || y > height )
+	if (x < 0 || y < 0 || x > width || y > height)
 		return;
-
 
 }
 
@@ -260,16 +259,15 @@ void drawLine(int x1, int y1, int x2, int y2)
 	if (x1 < 0 || y1 < 0 || x2 < 0 || y2 < 0 || x1 > width || y1 > height || x2 > width || y2 > height)
 		return;
 
-	if(x1>x2)
+	if (x1 > x2)
 	{
-		int tempX=x1;
-		int tempY=y1;
-		x1=x2;
-		y1=y2;
-		x2=tempX;
-		y2=tempY;
+		int tempX = x1;
+		int tempY = y1;
+		x1 = x2;
+		y1 = y2;
+		x2 = tempX;
+		y2 = tempY;
 	}
-
 
 	double rise = y2 - y1;
 	double run = x2 - x1;
@@ -277,7 +275,7 @@ void drawLine(int x1, int y1, int x2, int y2)
 	double nextX = x1;
 	double nexyY = y1;
 
-	while(nextX<=x2)
+	while (nextX <= x2)
 	{
 
 	}
@@ -285,132 +283,132 @@ void drawLine(int x1, int y1, int x2, int y2)
 
 void initScreen()
 {
-
+	unsigned char arr[] =
+	{ 0x00, 0xAF };
+	sendI2C(0x3C, arr, 2);
 }
-
-
 
 int main(void)
 {
 
-initSerial(0, 57600);
-initScreen();
+	initSerial(0, 57600);
+	initScreen();
 
-TWBR = 0x00;		//make it fast i guess
-TWSR &= (0xFC);		//clear prescaler bits
+	TWBR = 0x00;		//make it fast i guess
+	TWSR &= (0xFC);		//clear prescaler bits
 
-_delay_ms(500);
+	_delay_ms(500);
 
-clearScreen();
-updateDisplay();
+	clearScreen();
+	updateDisplay();
 
 //not sure about this
 //TWDR = 0xC0;	//0xC0 := databytes and commands requires
 //TWDR = 0x40;	//0x40 := stream of databytes
 //setContrast(0xFF);
-int i = 0;
-int j = 0;
-int y = 0;
-for (j = 0; j < 8; j++)
-	for (i = 0; i < 127; i++)
-	{
-		unsigned char bar[] =
-		{ 0x40, (1 << (y++ % 8)) };
-		sendI2C(0x3C, bar, 2);
-	}
-
-setContrast(0x00);
-_delay_ms(500);
-setContrast(0xFF);
-_delay_ms(500);
-setContrast(0x00);
-_delay_ms(500);
-setContrast(0xFF);
-_delay_ms(500);
-
-clearScreen();
-updateDisplay();
-
-setPinDirection(&DP12DDR, DP12MASK, 1);
-
-bufferIn[BUFFER_SIZE] = '\0';
-unsigned bip = 0;
-while (1)
-{
-	while (UCSR0A & (1 << RXC0))
-	{
-		unsigned char ch = USART_Receive();
-
-		bufferIn[bip++] = ch;
-		if (ch == '\r' || ch == '\n' || bip == BUFFER_SIZE)
+	int i = 0;
+	int j = 0;
+	int y = 0;
+	for (j = 0; j < 8; j++)
+		for (i = 0; i < 127; i++)
 		{
-			bufferIn[bip] = '\0';
-			sendStr("\r\n");
+			unsigned char bar[] =
+			{ 0x40, (1 << (y++ % 8)) };
+			sendI2C(0x3C, bar, 2);
+		}
+
+	setContrast(0x00);
+	_delay_ms(500);
+	setContrast(0xFF);
+	_delay_ms(500);
+	setContrast(0x00);
+	_delay_ms(500);
+	setContrast(0xFF);
+	_delay_ms(500);
+
+	clearScreen();
+	updateDisplay();
+
+	setPinDirection(&DP12DDR, DP12MASK, 1);
+
+	bufferIn[BUFFER_SIZE] = '\0';
+	unsigned bip = 0;
+	while (1)
+	{
+		while (UCSR0A & (1 << RXC0))
+		{
+			unsigned char ch = USART_Receive();
+
+			bufferIn[bip++] = ch;
+			if (ch == '\r' || ch == '\n' || bip == BUFFER_SIZE)
+			{
+				bufferIn[bip] = '\0';
+				sendStr("\r\n");
 //				sendStr(bufferIn);
-			//			sendStr("\r\n");
+				//			sendStr("\r\n");
 
-			unsigned char command[BUFFER_SIZE];
-			int cmdc = 0;
-			int ci = 0;
-			//unsigned c=0;
-			while (ci < bip)
-			{
-				int cmd;
+				unsigned char command[BUFFER_SIZE];
+				int cmdc = 0;
+				int ci = 0;
+				//unsigned c=0;
+				while (ci < bip)
+				{
+					int cmd;
 
-				char chr = bufferIn[ci++];
-				if (chr >= '0' && chr <= '9')
-					cmd = ((chr - '0') << 4);
-				else if (chr >= 'a' && chr <= 'f')
-					cmd = ((10 + chr - 'a') << 4);
-				else if (chr >= 'A' && chr <= 'F')
-					cmd = ((10 + chr - 'A') << 4);
-				else
-					continue;
+					char chr = bufferIn[ci++];
+					if (chr >= '0' && chr <= '9')
+						cmd = ((chr - '0') << 4);
+					else if (chr >= 'a' && chr <= 'f')
+						cmd = ((10 + chr - 'a') << 4);
+					else if (chr >= 'A' && chr <= 'F')
+						cmd = ((10 + chr - 'A') << 4);
+					else
+						continue;
 
-				//sendChar(ch);
-				chr = bufferIn[ci++];
+					//sendChar(ch);
+					chr = bufferIn[ci++];
 
-				if (chr >= '0' && chr <= '9')
-					cmd |= ((chr - '0'));
-				else if (chr >= 'a' && chr <= 'f')
-					cmd |= ((10 + chr - 'a'));
-				else if (chr >= 'A' && chr <= 'F')
-					cmd |= ((10 + chr - 'A'));
-				else
-					continue;
+					if (chr >= '0' && chr <= '9')
+						cmd |= ((chr - '0'));
+					else if (chr >= 'a' && chr <= 'f')
+						cmd |= ((10 + chr - 'a'));
+					else if (chr >= 'A' && chr <= 'F')
+						cmd |= ((10 + chr - 'A'));
+					else
+						continue;
 
-				//sendChar(ch);
+					//sendChar(ch);
 
-				//sprintf(str, "  cmd=%02x \r\n", cmd);
-				//sendStr(str);
-				command[cmdc++] = cmd;
+					//sprintf(str, "  cmd=%02x \r\n", cmd);
+					//sendStr(str);
+					command[cmdc++] = cmd;
+				}
+
+				bip = 0;
+
+				for (ci = 0; ci < cmdc; ci++)
+				{
+					sprintf(str, "%02x ", command[ci]);
+					sendStr(str);
+
+				}
+				sendStr("\r\n");
+				sendI2C(0x3C, command, cmdc);
+
+				bip = 0;
 			}
-
-			bip = 0;
-
-			for (ci = 0; ci < cmdc; ci++)
-			{
-				sprintf(str, "%02x ", command[ci]);
-				sendStr(str);
-
-			}
-			sendStr("\r\n");
-			sendI2C(0x3C, command, cmdc);
-
-			bip = 0;
 		}
 	}
-}
 
-while (1)
-{
-	DP13PORT |= (1 << DP13MASK);
-	writePin(&DP12PORT, DP12MASK, 1);
-	_delay_ms(500);
-	DP13PORT &= ~(1 << DP13MASK);
-	writePin(&DP12PORT, DP12MASK, 0);
-	_delay_ms(500);
+	while (1)
+	{
+		DP13PORT |= (1 << DP13MASK);
+		writePin(&DP12PORT, DP12MASK, 1);
+		_delay_ms(500);
+		DP13PORT &= ~(1 << DP13MASK);
+		writePin(&DP12PORT, DP12MASK, 0);
+		_delay_ms(500);
 
-};
-return 0;
+	};
+	return 0;
 }
