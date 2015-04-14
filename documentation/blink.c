905efb83998dfa29c5990d132c2c263fb728dada
jmt <jmt>
2015-04-14 00:45:32 -0500

    work

--- documentation/blink.c
+++ documentation/blink.c
@@ -1,4 +1,4 @@
-/* blink.c */
+/* blink.c -- For the UNO */
 
 #include <avr/io.h>
 #include <util/delay.h>
@@ -8,14 +8,14 @@ enum { BLINK_DELAY_MS = 1000 };
 int main (void)
 {
 
-  DDRB |= _BV(DDB7);                  /* set pin 5 of PORTB for output*/
+  DDRB |= _BV(DDB5);                  /* set pin 5 of PORTB for output*/
           
   while(1) 
     {
-      PORTB |= _BV(PORTB7);          /* set pin 5 high to turn led on */
+      PORTB |= _BV(PORTB5);          /* set pin 5 high to turn led on */
       _delay_ms(BLINK_DELAY_MS);
 
-      PORTB &= ~_BV(PORTB7);         /* set pin 5 low to turn led off */
+      PORTB &= ~_BV(PORTB5);         /* set pin 5 low to turn led off */
       _delay_ms(BLINK_DELAY_MS);
     }
 
