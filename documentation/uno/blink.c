cf7872a00af9a0349aad737e4c5a436df3648906
jmt <jmt>
2015-04-20 19:40:47 -0500

    final project donels!

---
+++ documentation/uno/blink.c
@@ -0,0 +1,24 @@
+/* blink.c -- For the Uno */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+enum { BLINK_DELAY_MS = 1000 };
+
+int main (void)
+{
+
+  DDRB |= _BV(DDB5);                /* set pin 5 of PORTB for output*/
+
+  while(1) 
+    {
+      PORTB |= _BV(PORTB5);        /* set pin 5 high to turn led on */
+      _delay_ms(BLINK_DELAY_MS);
+
+      PORTB &= ~_BV(PORTB5);       /* set pin 5 low to turn led off */
+      _delay_ms(BLINK_DELAY_MS);
+    }
+
+
+  return 0;                         /* CODE SHOULD NEVER REACH HERE */
+}
